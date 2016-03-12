394d5c2f4336f5e5425ff4defd3a155f234ebfeb
dhruvghulati <ghulatid@gmail.com>
2016-03-12 12:10:23 +0000

    Tried code to get notifications working

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'ngMaterial','veridu.angularjs.sdk','ngCordova','LocalStorageModule', 'starter.services', 'firebase'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngMaterial', 'veridu.angularjs.sdk', 'ngCordova', 'LocalStorageModule', 'starter.services', 'firebase'])
     .constant('FirebaseUrl', 'https://realex.firebaseio.com/')
     .service('rootRef', ['FirebaseUrl', Firebase])
 
@@ -15,16 +15,43 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngMaterial','veridu.
             if (window.cordova && window.cordova.plugins.Keyboard) {
                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
                 cordova.plugins.Keyboard.disableScroll(true);
-
             }
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
                 StatusBar.styleDefault();
             }
 
-        });
+        })
+
     })
 
+window.onNotification = function (e) {
+
+    switch (e.event) {
+        case 'registered':
+            if (e.regid.length > 0) {
+
+                var device_token = e.regid;
+                RequestsService.register(device_token).then(function (response) {
+                    alert('registered!');
+                });
+            }
+            break;
+
+        case 'message':
+            alert('msg received: ' + e.message);
+            break;
+
+        case 'error':
+            alert('error occured');
+            break;
+
+    }
+}
+
+
+
+//})
 
 
     .config(function ($stateProvider, $urlRouterProvider, VeriduProvider) {
@@ -32,21 +59,38 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngMaterial','veridu.
         VeriduProvider.client = 'demo';
         VeriduProvider.API_VERSION = 'rafael';
 
-        $stateProvider.state('app', {url: '/app', abstract: true, templateUrl: 'templates/menu.html', controller: 'AppCtrl'})
+        $stateProvider.state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppCtrl'
+            })
 
-            .state('app.veriduLogin', {url: '/veriduLogin', views: {'menuContent': {templateUrl: 'templates/veriduLogin.html',controller: 'AppCtrl'}}})
+            .state('app.veriduLogin', {
+                url: '/veriduLogin',
+                views: {'menuContent': {templateUrl: 'templates/veriduLogin.html', controller: 'AppCtrl'}}
+            })
 
             .state('app.response', {url: '/response', views: {'menuContent': {templateUrl: 'templates/response.html'}}})
 
             .state('app.realex', {url: '/realex', views: {'menuContent': {templateUrl: 'templates/realex.html'}}})
 
-            .state('app.profile', {url: '/profile/2', views: {'menuContent': {templateUrl: 'templates/profile.html', controller: 'ProfileCtrl'}}})
+            .state('app.profile', {
+                url: '/profile/2',
+                views: {'menuContent': {templateUrl: 'templates/profile.html', controller: 'ProfileCtrl'}}
+            })
 
             .state('app.search', {url: '/search', views: {'menuContent': {templateUrl: 'templates/search.html'}}})
             // .state('app.browse', { url: '/browse', views: { 'menuContent': { templateUrl: 'templates/browse.html' } } })
-            .state('app.playlists', {url: '/playlists',views: {'menuContent': {templateUrl: 'templates/playlists.html', controller: 'PlaylistsCtrl'}}})
+            .state('app.playlists', {
+                url: '/playlists',
+                views: {'menuContent': {templateUrl: 'templates/playlists.html', controller: 'PlaylistsCtrl'}}
+            })
 
-            .state('app.popup', {url: '/popup', views: {'menuContent': {templateUrl: 'templates/popup.html', controller: 'NotificationController'}}})
+            .state('app.popup', {
+                url: '/popup',
+                views: {'menuContent': {templateUrl: 'templates/popup.html', controller: 'NotificationController'}}
+            })
 
             .state('app.fblogin', {
                 url: '/fblogin',
@@ -77,9 +121,18 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngMaterial','veridu.
                     }
                 }
             })
-            .state('app.exhibits', { url: '/exhibits', views: { menuContent: { templateUrl: 'templates/exhibits.html', controller: 'ExhibitListCtrl' } }})
-            .state('app.exhibit', { url: '/exhibits/:exhibitId', views: { menuContent: {templateUrl: 'templates/exhibit.html', controller: 'ExhibitCtrl'}}})
-            .state('app.about', { url: '/about', views: { menuContent: {templateUrl: 'templates/about.html', controller: 'AboutCtrl'}}})
+            .state('app.exhibits', {
+                url: '/exhibits',
+                views: {menuContent: {templateUrl: 'templates/exhibits.html', controller: 'ExhibitListCtrl'}}
+            })
+            .state('app.exhibit', {
+                url: '/exhibits/:exhibitId',
+                views: {menuContent: {templateUrl: 'templates/exhibit.html', controller: 'ExhibitCtrl'}}
+            })
+            .state('app.about', {
+                url: '/about',
+                views: {menuContent: {templateUrl: 'templates/about.html', controller: 'AboutCtrl'}}
+            })
 
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/app/exhibits');
--- www/js/background.js
+++ www/js/background.js
@@ -1,3 +1,6 @@
+
+
+
 // Run when the device is ready
 document.addEventListener("deviceready", function () {
 
@@ -6,27 +9,108 @@ document.addEventListener("deviceready", function () {
     // The plug-in has to create a notification while in background - like a download progress bar.
     cordova.plugins.backgroundMode.setDefaults({
         title: 'Invest in this artwork!!',
-        text: 'A very exciting artwork to invest in',
+        text: 'A very exciting artwork to invest in'
         //resume: false
     });
 
     // Enable background mode
     cordova.plugins.backgroundMode.enable();
 
-    cordova.plugins.backgroundMode.onfailure = function(errorCode) {};
+    cordova.plugins.backgroundMode.onfailure = function (errorCode) {
+    };
 
     // Called when background mode has been activated
     cordova.plugins.backgroundMode.onactivate = function () {
-
+        //var id = guid();
         // Set an interval of 30 minutes (1800000 milliseconds)
         setInterval(function () {
-
             cordova.plugins.backgroundMode.configure({
                 title: 'Invest in this artwork!! :)'
             })
         }, 5000);
+
+        //onNotificationGCM(id);
+
+
     }
 }, false);
 /**
  * Created by dhruv on 12/03/2016.
  */
+
+//************************************  OUTSIDE DEVICE READY
+// handle APNS notifications for iOS
+//function onNotificationAPN(e) {
+//    // storage the e.id value  (the extra value sent in push notification)
+//    window.localStorage.setItem("push_que", e.id);
+//    var push_que = e.id;
+//    // if the push notification is coming inline
+//    if (e.foreground == "1") {
+//        // storage the e.numero value  (the extra value sent in push notification)
+//        window.localStorage.setItem("push_que", e.id);
+//        var push_que = e.id;
+//        /// some code here to open a message  if a new push is recieved inline
+//        ;
+//    }
+//    if (event.alert) {
+//        navigator.notification.alert(event.alert);
+//    }
+//    if (event.sound) {
+//        var snd = new Media(event.sound);
+//        snd.play();
+//    }
+//    if (event.badge) {
+//        pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, event.badge);
+//    }
+//}
+//
+//// handle GCM notifications for Android
+//function onNotificationGCM(e) {
+//    switch (e.event) {
+//        if
+//            (e.foreground)
+//        {
+//            //  if the push is recieved inline
+//            //  storage the value of  playoad.id,  the extra value sent by push
+//            window.localStorage.setItem("push_que", e.payload.id);
+//            var push_que = e.payload.id;
+//
+//        }
+//        else
+//        {
+//            // otherwise we were launched because the user touched a notification in the notification tray
+//
+//            if (e.coldstart) {
+//                //  storage the value of  playoad.numero, the extra value sent by push
+//                window.localStorage.setItem("push_que", e.payload.id);
+//
+//            }
+//            else {
+//                //  storage the value of  playoad.numero, the extra value sent by push
+//                window.localStorage.setItem("push_que", e.payload.id);
+//
+//            }
+//        }
+//
+//            break;
+//
+//        case 'error':
+//            break;
+//        default:
+//            break;
+//    }
+//}
+//
+//function guid() {
+//    function s4() {
+//        return Math.floor((1 + Math.random()) * 0x10000)
+//            .toString(16)
+//            .substring(1);
+//    }
+//    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+//        s4() + '-' + s4() + s4() + s4();
+//}
+//
+//
+//
+////********************************** END OUTSIDE DEVICE READY
