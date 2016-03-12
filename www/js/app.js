b83bf071a7028ead6cbb1b68b9b07525a042bb54
dhruvghulati <ghulatid@gmail.com>
2016-03-12 05:30:12 +0000

    Merge branch 'master' into dhruv
    # Conflicts:
    #	www/js/controllers.js

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'firebase'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngMaterial', 'starter.services', 'firebase'])
     .constant('FirebaseUrl', 'https://realex.firebaseio.com/')
     .service('rootRef', ['FirebaseUrl', Firebase])
 
@@ -83,15 +83,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
                     }
                 }
             })
-
-            .state('app.browse', {
-                url: '/browse',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'templates/browse.html'
-                    }
-                }
-            })
+            // .state('app.browse', { url: '/browse', views: { 'menuContent': { templateUrl: 'templates/browse.html' } } })
             .state('app.playlists', {
                 url: '/playlists',
                 views: {
@@ -130,7 +122,11 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
                         controller: 'PlaylistCtrl'
                     }
                 }
-            });
+            })
+            .state('app.exhibits', { url: '/exhibits', views: { menuContent: { templateUrl: 'templates/exhibits.html', controller: 'ExhibitListCtrl' } }})
+            .state('app.exhibit', { url: '/exhibits/:exhibitId', views: { menuContent: {templateUrl: 'templates/exhibit.html', controller: 'ExhibitCtrl'}}})
+            .state('app.about', { url: '/about', views: { menuContent: {templateUrl: 'templates/about.html', controller: 'AboutCtrl'}}})
+
         // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/app/playlists');
+        $urlRouterProvider.otherwise('/app/exhibits');
     });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,6 @@
 angular.module('starter.controllers', ['firebase','starter.services'])
 
+<<<<<<< HEAD
     .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
         // With the new view caching in Ionic, Controllers are only called
@@ -97,6 +98,117 @@ angular.module('starter.controllers', ['firebase','starter.services'])
 function LoginCtrl(Auth, $state) {
     this.loginWithFacebook = function loginWithFacebook() {
         Auth.$authWithOAuthPopup('facebook')
+=======
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.factory('ExhibitFactory', function($http) {
+
+  function get(cb, id) {
+
+    var arr = [ // TODO Image, Amount Sold
+      { id: 1, name: 'Shells on a Sea of Green', artist: 'Sam Smith', price: '120.45', imagePath: '/img/shells.jpeg', description: 'This seminal work by new artist Sam Smith defines a generation of excess and decadance by famous artists like Van Gogh and Salvador Dali. Inspired by his mother\'s hair loss at a young age.' },
+      { id: 2, name: 'A plaintiff among stars', artist: 'Sam Smith', price: '865.95', imagePath: '/img/star.jpg', description: 'This is our favourite piece of art. If you don\'t invest, we will steal your first born.' },
+      { id: 3, name: 'Cat got your tongue', artist: 'Jessica Bean Heinz', price: '465.95', imagePath: '/img/cat.jpg', description: 'She is truly a wonder to behold, all her work is magnificent.' }
+    ];
+
+    if (id !== undefined) {
+      var g = arr.filter(function(e) { return e.id == id });
+      arr = g;
+    }
+
+    // TODO This would return a real API response of users. Would be async, so need to .then(cb)
+    cb(arr);
+  }
+
+  return {
+    get: get
+  };
+
+})
+
+.controller('ExhibitListCtrl', function($scope, ExhibitFactory) {
+
+  ExhibitFactory.get(function(response) {
+    $scope.exhibits = response;
+    // TODO Any UI parameters we need to add?
+  });
+
+  $scope.love = function(exhibit) {
+    exhibit.love = !exhibit.love || true;
+  }
+
+})
+
+.controller('ExhibitCtrl', function($scope, ExhibitFactory, $stateParams) {
+
+  ExhibitFactory.get(function(response) {
+    $scope.exhibit = response[0];
+    // TODO Any UI parameters we need to add?
+  }, $stateParams.exhibitId);
+  console.log($scope.exhibit);
+  // ExhibitFactory.get(id)
+  // $scope.exhibit = ExhibitFactory
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+
+})
+
+.controller('LoginCtrl', function(Auth, $state) {
+
+  this.loginWithFacebook = function loginWithFacebook() {
+    Auth.$authWithOAuthPopup('facebook')
+>>>>>>> master
             .then(function (authData) {
                 console.log(authData);
                 var ref = new Firebase("https://realex.firebaseio.com");
@@ -115,5 +227,53 @@ function LoginCtrl(Auth, $state) {
                 $state.go('app.creditcard');
             });
     };
-}
-LoginCtrl.$inject = ['Auth', '$state'];
+})
+
+.controller('PlaylistsCtrl', function ($scope) {
+    $scope.playlists = [
+        {title: 'Reggae', id: 1},
+        {title: 'Chill', id: 2},
+        {title: 'Dubstep', id: 3},
+        {title: 'Indie', id: 4},
+        {title: 'Rap', id: 5},
+        {title: 'Cowbell', id: 6}
+    ];
+})
+
+.controller('PlaylistCtrl', function ($scope, $stateParams) {
+
+})
+
+.controller('AboutCtrl', function ($scope, $stateParams) {
+
+})
+
+.controller('CreditCtrl', ['$scope', '$rootScope', '$firebaseAuth', '$window',
+    function ($scope) {
+        $scope.card = {
+            id: "",
+            number: "",
+            expiry: "",
+            cvv: ""
+        };
+
+        $scope.createCard = function () {
+            var id = this.card.id;
+            var number = this.card.number;
+            var expiry = this.card.expiry;
+            var cvv = this.card.cvv;
+
+            var ref = new Firebase("https://realex.firebaseio.com");
+            var cardRef = ref.child("cards");
+            cardRef.push().set(
+                {
+                    id: id,
+                    number: number,
+                    expiry: expiry,
+                    cvv: cvv
+                }
+            );
+            $state.go('app.playlists');
+
+        }
+    }]);
